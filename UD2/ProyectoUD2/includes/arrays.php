98eeabd3435b8dbce3eac5c044f4de3c3c4a1253
RSAdrian9 <adrian4rs@gmail.com>
2024-11-11 07:24:51 +0100

    Update files

---
+++ UD2/ProyectoUD2/includes/arrays.php
@@ -0,0 +1,31 @@
+<?php
+
+// Array de juegos
+$games = [
+    ["titulo" => "The Legend of Zelda", "plataforma" => "Nintendo Switch", "precio" => 59.99, "imagen" => "assets/images/zelda.jpg"],
+    ["titulo" => "God of War", "plataforma" => "PlayStation 5", "precio" => 49.99, "imagen" => "assets/images/godofwar.jpg"],
+    ["titulo" => "Halo Infinite", "plataforma" => "Xbox Series X", "precio" => 39.99, "imagen" => "assets/images/halo.jpg"],
+    ["titulo" => "Minecraft", "plataforma" => "Multiplataforma", "precio" => 29.99, "imagen" => "assets/images/minecraft.jpg"],
+    ["titulo" => "Cyberpunk 2077", "plataforma" => "PC", "precio" => 44.99, "imagen" => "assets/images/cyberpunk.jpg"],
+    ["titulo" => "Fortnite", "plataforma" => "Multiplataforma", "precio" => 0.00, "imagen" => "assets/images/fortnite.jpg"]
+];
+
+
+// Array de géneros
+$genres = [
+    ["title" => "Acción", "description" => "Intensas aventuras llenas de desafíos.", "image" => "images/accion.jpg"],
+    ["title" => "Aventura", "description" => "Explora mundos y resuelve misterios.", "image" => "images/aventura.jpg"],
+    ["title" => "Deportes", "description" => "Emoción de los deportes populares.", "image" => "images/deportes.jpg"],
+    ["title" => "Estrategia", "description" => "Toma decisiones estratégicas.", "image" => "images/estrategia.jpg"],
+    ["title" => "Rol", "description" => "Historias inmersivas y personajes personalizados.", "image" => "images/rol.jpg"],
+];
+
+// Array de consolas
+$consoles = [
+    ["title" => "PlayStation", "description" => "Juegos exclusivos y multiplataforma.", "image" => "images/playstation.jpg"],
+    ["title" => "Xbox", "description" => "Juegos de gran rendimiento en Xbox.", "image" => "images/xbox.jpg"],
+    ["title" => "Nintendo Switch", "description" => "Juegos portátiles y exclusivos.", "image" => "images/switch.jpg"],
+    ["title" => "PC", "description" => "La máxima potencia y gráficos.", "image" => "images/pc.jpg"],
+];
+
+?>
