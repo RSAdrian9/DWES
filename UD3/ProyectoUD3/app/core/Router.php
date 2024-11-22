13490355d83b812019945b5e047986c691e31056
RSAdrian9 <adrian4rs@gmail.com>
2024-11-22 12:28:16 +0100

    221124 - UD4/5CRUD_MVC

--- UD3/ProyectoUD3/app/core/Router.php
+++ UD3/ProyectoUD3/app/core/Router.php
@@ -2,29 +2,44 @@
 namespace App\Core;
 
 class Router {
+    protected $routes = [];
+
+    public function __construct() {
+        // Definir rutas básicas
+        $this->routes = [
+            '/' => ['HomeController', 'index'],
+            '/about' => ['PageController', 'about'],
+            '/contact' => ['PageController', 'contact']
+        ];
+    }
+
     public function run() {
-        $url = $_GET['url'] ?? '/';
-        
-        // Interpretar la URL
-        $parts = explode('/', trim($url, '/'));
-        $controllerName = !empty($parts[0]) ? ucfirst($parts[0]) . 'Controller' : 'HomeController';
-        $methodName = $parts[1] ?? 'index';
-        
-        $controllerFile = "../app/controllers/$controllerName.php";
-        
-        if (file_exists($controllerFile)) {
-            require_once $controllerFile;
-            $controller = new $controllerName();
-            
-            if (method_exists($controller, $methodName)) {
-                $controller->$methodName();
+        $url = $this->getUrl();
+
+        // Buscar la ruta correspondiente
+        if (isset($this->routes[$url])) {
+            [$controllerName, $methodName] = $this->routes[$url];
+
+            // Cargar el controlador
+            $controllerClass = "App\\Controllers\\$controllerName";
+            if (class_exists($controllerClass) && method_exists($controllerClass, $methodName)) {
+                $controller = new $controllerClass();
+                call_user_func([$controller, $methodName]);
             } else {
-                echo "Método no encontrado.";
+                $this->error404();
             }
         } else {
-            echo "Controlador no encontrado.";
+            $this->error404();
         }
     }
-}
 
-?>
+    protected function getUrl() {
+        $url = $_SERVER['REQUEST_URI'];
+        $url = parse_url($url, PHP_URL_PATH); // Ignorar query strings
+        return rtrim($url, '/'); // Eliminar barra final
+    }
+
+    protected function error404() {
+        echo "404 - Página no encontrada";
+    }
+}
